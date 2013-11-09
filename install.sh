dea2530b32d778272ce4f66b969bba307686d4e7
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2013-11-08 16:19:30 -0800

    Vim plugins folder

--- install.sh
+++ install.sh
@@ -7,12 +7,14 @@ cp ~/.bashrc ~/.bashrc.`date +%T`.backup
 cp ~/.bash_profile ~/.bash_profile.`date +%T`.backup
 cp ~/.inputrc ~/.inputrc.`date +%T`.backup
 cp ~/.vimrc ~/.vimrc.`date +%T`.backup
+cp -r ~/.vim ~/.vim.`date +%T`.backup
 
 # copy new ones:
 cp bashrc ~/.bashrc
 cp bash_profile ~/.bash_profile
 cp inputrc ~/.inputrc
 cp vimrc ~/.vimrc
+cp -r vim ~/.vim
 
 # source them:
 source ~/.bashrc
